67c3a8233af83697ffbda43efa8e05c52fe0c2dc
Mohamed Nagy <106141020+Ged0oo@users.noreply.github.com>
2023-09-11 14:13:31 +0300

    Update Std_Types.h

--- Std_Types.h
+++ Std_Types.h
@@ -1,43 +1,34 @@
  /******************************************************************************
- * @File :				Std_Types.h
- * @Version :			1.0.0
- * @Module: 			Dio
- * @brief : 			Header file for Dio Module.
- * @Author : 			Mohamed Nagy
+ * @File :				Std_Types.h                                            *
+ * @Version :			1.0.0                                                  *
+ * @Module: 			Dio                                                    *
+ * @brief : 			Header file for Dio Module.                            *
+ * @Author : 			Mohamed Nagy                                           *
  ******************************************************************************/
- 
+
  /******************************************************************************
- * Project :			AUTOSAR r22-11 MCAL
- * Platform :			ARM
- * Board : 				STM32F103C8T6
- * Autosar Version :	4.4.0
- * SW Version : 		1.0.0
+ * Project :			AUTOSAR 4.4.0 MCAL 								       *
+ * Platform :			AVR                 								   *
+ * Board : 				ATMEGA32       								           *
+ * Autosar Version :	4.4.0               								   *
+ * SW Version : 		1.0.0               								   *
  ******************************************************************************/
 
-#ifndef __STD_TYPES_H__
-#define __STD_TYPES_H__
+
+#ifndef STD_TYPES_H
+#define STD_TYPES_H
+
+
+# include "Platform_Types.h"
+# include "Compiler.h"
+
 
 /*
- * @brief Include Platform Abstraction
+ * Id for the company in the AUTOSAR
+ * for example GCC ID = 1000
  */
-#include "Platform_Types.h"
+#define STD_TYPES_VENDOR_ID                                  (1000U)
 
-/*
- * @brief Include Compiler Abstraction
- */
-#include "Compiler.h"
-
-/* 
- * @brief Id for the company in the AUTOSAR
- * for example Elektrobit ID = 1  
- */
-#define STD_TYPES_VENDOR_ID                                  (1U)
-
-/* 
- * @brief Id for the company in the AUTOSAR
- * for example Elektrobit ID = 1  
- */
-#define COMPILER_VENDOR_ID                                  (1U)
 
 /*
  * Module Version 1.0.0
@@ -46,31 +37,72 @@
 #define STD_TYPES_SW_MINOR_VERSION                           (0U)
 #define STD_TYPES_SW_PATCH_VERSION                           (0U)
 
+
 /*
- * AUTOSAR Version 4.4.0
+ * AUTOSAR Version 4.0.3
  */
 #define STD_TYPES_AR_RELEASE_MAJOR_VERSION                   (4U)
-#define STD_TYPES_AR_RELEASE_MINOR_VERSION                   (4U)
-#define STD_TYPES_AR_RELEASE_PATCH_VERSION                   (0U)
+#define STD_TYPES_AR_RELEASE_MINOR_VERSION                   (0U)
+#define STD_TYPES_AR_RELEASE_PATCH_VERSION                   (3U)
 
-#ifndef DISABLE_MCAL_INTERMODULE_ASR_CHECK
-#if((STD_TYPES_AR_MAJOR_VERSION != PLATFORM_TYPES_AR_MAJOR_VERSION) || \
-	(STD_TYPES_AR_MINOR_VERSION != PLATFORM_TYPES_AR_MINOR_VERSION))
-	#error "Autosar Version Numbers of Std_Types.h and Platform_Types.h are different"
-#endif
-#endif
 
-#ifndef DISABLE_MCAL_INTERMODULE_ASR_CHECK
-#if((STD_TYPES_AR_MAJOR_VERSION != COMPILER_AR_MAJOR_VERSION) || \
-	(STD_TYPES_AR_MINOR_VERSION != COMPILER_AR_MINOR_VERSION))
-	#error "Autosar Version Numbers of Std_Types.h and Compiler.h are different"
-#endif
-#endif
+#define STD_HIGH        0x01U       /* Standard HIGH */
+#define STD_LOW         0x00U       /* Standard LOW */
+
+
+#define LOGIC_HIGH        0x01U       /* Standard HIGH */
+#define LOGIC_LOW         0x00U       /* Standard LOW */
+
+
+#define STD_ACTIVE      0x01U       /* Logical state active */
+#define STD_IDLE        0x00U       /* Logical state idle */
+
+
+#define STD_ON          0x01U       /* Standard ON */
+#define STD_OFF         0x00U       /* Standard OFF */
+
+
+typedef uint8         Std_ReturnType;
+#define E_OK            ((Std_ReturnType)0x00U)      /* Function Return OK */
+#define E_NOT_OK        ((Std_ReturnType)0x01U)      /* Function Return NOT OK */
+
 
 /*
- *  Describes the standard Return Type Definitions used in the project
+ * The memory class is used for the declaration of local pointers
  */
-typedef uint8  Std_ReturnType;
+#define AUTOMATIC
+
+
+/*
+ *  The memory class is used within type definitions,
+ *   where no memory qualifier can be specified
+ */
+#define TYPEDEF
+
+
+/*
+ *  This is used to define the void pointer to zero definition
+ */
+#define NULL_PTR          ((void *)0)
+
+
+/*
+ *  This is used to define the abstraction of compiler keyword inline
+ */
+#define INLINE            inline
+
+
+/*
+ *  This is used to define the local inline function
+ */
+#define LOCAL_INLINE      static inline
+
+
+/*
+ *  This is used to define the abstraction of compiler keyword static
+ */
+#define STATIC            static
+
 
 /*
  * Structure for the Version of the module.
@@ -85,21 +117,5 @@ typedef struct
   uint8 sw_patch_version;
 } Std_VersionInfoType;
 
-#define STD_HIGH        0x01U       /* Standard HIGH */
-#define STD_LOW         0x00U       /* Standard LOW */
 
-#define LOGIC_HIGH      0x01U       /* Standard HIGH */
-#define LOGIC_LOW       0x00U       /* Standard LOW */
-
-#define STD_ACTIVE      0x01U       /* Logical state active */
-#define STD_IDLE        0x00U       /* Logical state idle */
-
-#define STD_ON          0x01U       /* Standard ON */
-#define STD_OFF         0x00U       /* Standard OFF */
-
-#define E_OK            ((Std_ReturnType)0x00U)      /* Function Return OK */
-#define E_NOT_OK        ((Std_ReturnType)0x01U)      /* Function Return NOT OK */
-
-
-#endif //__STD_TYPES_H__
-
+#endif /* STD_TYPES_H */
