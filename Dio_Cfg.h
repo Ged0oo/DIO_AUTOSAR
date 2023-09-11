a2aa5132705b4d0d60ce5a1a75f130621373d186
Mohamed Nagy <106141020+Ged0oo@users.noreply.github.com>
2023-09-11 14:12:18 +0300

    Update Dio_Cfg.h

--- Dio_Cfg.h
+++ Dio_Cfg.h
@@ -1,68 +1,112 @@
-/******************************************************************************
- * @File :				Dio_Cfg.h
- * @Version :			1.0.0
- * @Module: 			Dio
- * @brief : 			Header file for Dio Module.
- * @Author : 			Mohamed Nagy
- ******************************************************************************/
- 
  /******************************************************************************
- * Project :			AUTOSAR r22-11 MCAL
- * Platform :			ARM
- * Board : 				STM32F103C8T6
- * Autosar Version :	4.4.0
- * SW Version : 		1.0.0
+ * @File :				Dio_Cfg.h                                              *
+ * @Version :			1.0.0                                                  *
+ * @Module: 			Dio                                                    *
+ * @brief : 			Header file for Dio Module.                            *
+ * @Author : 			Mohamed Nagy                                           *
  ******************************************************************************/
- 
-#ifndef __DIO_CFG_H__
-#define __DIO_CFG_H__
+
+ /******************************************************************************
+ * Project :			AUTOSAR 4.4.0 MCAL 								       *
+ * Platform :			AVR                 								   *
+ * Board : 				ATMEGA32       								           *
+ * Autosar Version :	4.4.0               								   *
+ * SW Version : 		1.0.0               								   *
+ ******************************************************************************/
+
+
+/* =====================================================================================================
+ * 											 FILE GUARD
+ * =====================================================================================================*/
+
+#ifndef DIO_CFG_H
+#define DIO_CFG_H
+
+
+/* =====================================================================================================
+ * 										VERSION DEFINITION
+ * =====================================================================================================*/
 
 /*
  * Module Version 1.0.0
  */
-#define DIO_CFG_SW_MAJOR_VERSION                           (1U)
-#define DIO_CFG_SW_MINOR_VERSION                           (0U)
-#define DIO_CFG_SW_PATCH_VERSION                           (0U)
+#define DIO_CFG_SW_MAJOR_VERSION              (1U)
+#define DIO_CFG_SW_MINOR_VERSION              (0U)
+#define DIO_CFG_SW_PATCH_VERSION              (0U)
+
 
 /*
- * AUTOSAR Version 4.4.0
+ * AUTOSAR Version 4.0.3
  */
-#define DIO_CFG_AR_RELEASE_MAJOR_VERSION                   (4U)
-#define DIO_CFG_AR_RELEASE_MINOR_VERSION                   (4U)
-#define DIO_CFG_AR_RELEASE_PATCH_VERSION                   (0U)
+#define DIO_CFG_AR_RELEASE_MAJOR_VERSION     (4U)
+#define DIO_CFG_AR_RELEASE_MINOR_VERSION     (0U)
+#define DIO_CFG_AR_RELEASE_PATCH_VERSION     (3U)
+
+
+/* =====================================================================================================
+ * 											INCLUDES
+ * =====================================================================================================*/
+
+/*
+ *  Standard AUTOSAR types
+ */
+#include "Std_Types.h"
+
+
+/* =====================================================================================================
+ * 								          VERSION CHECK
+ * =====================================================================================================*/
+
+/*
+ * AUTOSAR checking between Std Types and Dio Modules
+ */
+#if ((STD_TYPES_AR_RELEASE_MAJOR_VERSION != DIO_CFG_AR_RELEASE_MAJOR_VERSION)\
+ ||  (STD_TYPES_AR_RELEASE_MINOR_VERSION != DIO_CFG_AR_RELEASE_MINOR_VERSION)\
+ ||  (STD_TYPES_AR_RELEASE_PATCH_VERSION != DIO_CFG_AR_RELEASE_PATCH_VERSION))
+  #error "The AR version of Std_Types.h does not match the expected version"
+#endif
+
+
+/* =====================================================================================================
+ * 								    Configuration Parameters
+ * =====================================================================================================*/
+
+/*
+ * Pre-compile option for Development Error Detect
+ */
+#define DIO_DEV_ERROR_DETECT                (STD_OFF)
+
+
+/*
+ * Pre-compile option for Version Info API
+ */
+#define DIO_VERSION_INFO_API                (STD_ON)
+
 
 #define INPUT                              (uint8)0x00
 #define OUTPUT                             (uint8)0x01
 #define PIN_INPUT                          (uint8)0x00
 #define PIN_OUTPUT                         (uint8)0x01
 
-/* Pre-compile option for Development Error Detect */
-#define DIO_DEV_ERROR_DETECT                (STD_ON)
 
-/* Pre-compile option for Version Info API */
-#define DIO_VERSION_INFO_API                (STD_ON)
-
-/* Pre-compile option for presence of Dio_FlipChannel API */
-#define DIO_FLIP_CHANNEL_API                (STD_OFF)
-
-/* Number of the configured Dio Channels */
-#define DIO_CONFIGURED_CHANNLES             (4U)
-
-/* Number of the configured Dio Ports */
-#define DIO_CONFIGURED_PORTS 				(32U)
-
-/* Number of the configured Dio Groups */
-#define DIO_CONFIGURED_GROUPS 				(0U)
-
-/* Number of channels per Dio Port */
+/*
+ * Number of the configured Dio Channels
+ */
+#define DIO_CONFIGURED_PORTS       	        (4U)
+#define DIO_TOTAL_CHANNLES        	        (32U)
+#define DIO_CONFIGURED_CHANNLES             (32U)
 #define NUM_OF_PINS_IN_SINGLE_PORT			(8U)
+
 
 #define PORTA_ID                          (uint8)0x01
 #define PORTB_ID                          (uint8)0x02
 #define PORTC_ID                          (uint8)0x03
 #define PORTD_ID                          (uint8)0x04
 
-/* Port Input Pins */
+
+/*
+ * Port Input Pins
+ */
 #define    PIN0         0
 #define    PIN1         1
 #define    PIN2         2
@@ -72,7 +116,10 @@
 #define    PIN6         6
 #define    PIN7         7
 
-/* Port Input Pins */
+
+/*
+ * Port Input Pins
+ */
 #define    PIN0_ID         0
 #define    PIN1_ID         1
 #define    PIN2_ID         2
@@ -82,7 +129,10 @@
 #define    PIN6_ID         6
 #define    PIN7_ID         7
 
-/* Port Input Pins */
+
+/*
+ * Port Input Pins
+ */
 #define    PIN_A_0         0  // 0
 #define    PIN_A_1         1  // 1
 #define    PIN_A_2         2  // 2
@@ -91,7 +141,6 @@
 #define    PIN_A_5         5  // 5
 #define    PIN_A_6         6  // 6
 #define    PIN_A_7         7  // 7
-
 #define    PIN_B_0         8  // 0
 #define    PIN_B_1         9  // 1
 #define    PIN_B_2         10 // 2
@@ -100,7 +149,6 @@
 #define    PIN_B_5         13 // 5
 #define    PIN_B_6         14 // 6
 #define    PIN_B_7         15 // 7
-
 #define    PIN_C_0         16 // 0
 #define    PIN_C_1         17 // 1
 #define    PIN_C_2         18 // 2
@@ -109,7 +157,6 @@
 #define    PIN_C_5         21 // 5
 #define    PIN_C_6         22 // 6
 #define    PIN_C_7         23 // 7
-
 #define    PIN_D_0         24 // 0
 #define    PIN_D_1         25 // 1
 #define    PIN_D_2         26 // 2
@@ -119,4 +166,4 @@
 #define    PIN_D_6         30 // 6
 #define    PIN_D_7         31 // 7
 
-#endif //__DIO_CFG_H__
+#endif /* DIO_CFG_H */
