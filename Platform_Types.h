28ace79e21e805824910199be85b4c404d6cc4a6
Mohamed Nagy <106141020+Ged0oo@users.noreply.github.com>
2023-09-11 14:13:16 +0300

    Update Platform_Types.h

--- Platform_Types.h
+++ Platform_Types.h
@@ -1,90 +1,112 @@
  /******************************************************************************
- * @File :				Dio.h
- * @Version :			1.0.0
- * @Module: 			Dio
- * @brief : 			Header file for Dio Module.
- * @Author : 			Mohamed Nagy
- ******************************************************************************/
- 
-  /******************************************************************************
- * Project :			AUTOSAR r22-11 MCAL
- * Platform :			ARM
- * Board : 				STM32F103C8T6
- * Autosar Version :	4.4.0
- * SW Version : 		1.0.0
+ * @File :				platform_types.h                                       *
+ * @Version :			1.0.0                                                  *
+ * @Module: 			Dio                                                    *
+ * @brief : 			Header file for Dio Module.                            *
+ * @Author : 			Mohamed Nagy                                           *
  ******************************************************************************/
 
-#ifndef __PLATFORM_TYPES_H__
-#define __PLATFORM_TYPES_H__
+ /******************************************************************************
+ * Project :			AUTOSAR 4.4.0 MCAL 								       *
+ * Platform :			AVR                 								   *
+ * Board : 				ATMEGA32       								           *
+ * Autosar Version :	4.4.0               								   *
+ * SW Version : 		1.0.0               								   *
+ ******************************************************************************/
+
+#ifndef PLATFORM_TYPES_H
+#define PLATFORM_TYPES_H
+
 
 /*
- * @brief COMPILER VENDOR ID 
+ * Id for the company in the AUTOSAR
+ * for example GCC ID = 1000 :)
  */
-#define PLATFORM_TYPES_VENDOR_ID                                  (1U)
+#define PLATFORM_VENDOR_ID                          (1000U)
+
 
 /*
  * Module Version 1.0.0
  */
-#define PLATFORM_TYPES_SW_MAJOR_VERSION                           (1U)
-#define PLATFORM_TYPES_SW_MINOR_VERSION                           (0U)
-#define PLATFORM_TYPES_SW_PATCH_VERSION                           (0U)
+#define PLATFORM_SW_MAJOR_VERSION                   (1U)
+#define PLATFORM_SW_MINOR_VERSION                   (0U)
+#define PLATFORM_SW_PATCH_VERSION                   (0U)
+
 
 /*
- * AUTOSAR Version 4.4.0
+ * AUTOSAR Version 4.0.3
  */
-#define PLATFORM_TYPES_AR_RELEASE_MAJOR_VERSION                   (4U)
-#define PLATFORM_TYPES_AR_RELEASE_MINOR_VERSION                   (4U)
-#define PLATFORM_TYPES_AR_RELEASE_PATCH_VERSION                   (0U)
+#define PLATFORM_AR_RELEASE_MAJOR_VERSION           (4U)
+#define PLATFORM_AR_RELEASE_MINOR_VERSION           (0U)
+#define PLATFORM_AR_RELEASE_PATCH_VERSION           (3U)
+
 
 /*
  * CPU register type width
  */
-#define CPU_TYPE_8                                  			  (8U)
-#define CPU_TYPE_16                                 			  (16U)
-#define CPU_TYPE_32                                 			  (32U)
+#define CPU_TYPE_8                                  (8U)
+#define CPU_TYPE_16                                 (16U)
+#define CPU_TYPE_32                                 (32U)
+
 
 /*
- * Platform type and endianess definitions, specific for AVR
+ * Platform type and endianess definitions, specific for ARM Cortex-M4F
  */
-#define CPU_TYPE            									  CPU_TYPE_8
+#define CPU_TYPE            CPU_TYPE_32
 
-/*
- * Bit order definition
- */
-#define MSB_FIRST                   (0u)        /* Big endian bit ordering        */
-#define LSB_FIRST                   (1u)        /* Little endian bit ordering     */
 
-/*
- * Byte order definition
- */
-#define HIGH_BYTE_FIRST             (0u)        /* Big endian byte ordering       */
-#define LOW_BYTE_FIRST              (1u)        /* Little endian byte ordering    */
+#define CPU_BIT_ORDER    LSB_FIRST        /*little endian bit ordering*/
+#define CPU_BYTE_ORDER   LOW_BYTE_FIRST   /*little endian byte ordering*/
 
-#define CPU_BIT_ORDER       		LSB_FIRST
-#define CPU_BYTE_ORDER      		LOW_BYTE_FIRST
 
-/*
- * Boolean Values
- */
-#ifndef FALSE
-#define FALSE       				(0u)
-#endif				
-				
-#ifndef TRUE				
-#define TRUE        				(1u)
+#define WORD_LENGTH_BITS      32u
+#define WORD_LENGTH_BYTES     4u
+#define MSB_FIRST             0u    /* big endian bit ordering */
+#define LSB_FIRST			  1u    /* little endian bit ordering */
+
+
+#define HIGH_BYTE_FIRST  0u    /* big endian byte ordering */
+#define LOW_BYTE_FIRST   1u    /* little endian byte ordering */
+
+
+#ifndef TRUE
+#define TRUE   1u
 #endif
 
-typedef unsigned char         		boolean;
-typedef unsigned char         		uint8;  
-typedef signed char           		sint8;  
-typedef unsigned short        		uint16; 
-typedef signed short          		sint16; 
-typedef unsigned long         		uint32; 
-typedef signed long           		sint32; 
-typedef unsigned long long    		uint64; 
-typedef signed long long      		sint64; 
-typedef float                 		float32;
-typedef double                		float64;
 
-#endif //__COMPILER_H__
+#ifndef FALSE
+#define FALSE  0u
+#endif
 
+
+#define ENABLE    1u
+#define DISABLE   0u
+
+
+typedef unsigned char         boolean;       /*        TRUE .. FALSE           */
+typedef signed char           sint8;         /*        -128 .. +127            */
+typedef unsigned char         uint8;         /*           0 .. 255             */
+typedef signed short          sint16;        /*      -32768 .. +32767          */
+typedef unsigned short        uint16;        /*           0 .. 65535           */
+typedef signed long           sint32;        /* -2147483648 .. +2147483647     */
+typedef unsigned long         uint32;        /*           0 .. 4294967295      */
+typedef float                 float32;
+typedef double                float64;
+typedef signed char           sint8_t;         /*        -128 .. +127            */
+typedef unsigned char         uint8_t;         /*           0 .. 255             */
+typedef signed short          sint16_t;        /*      -32768 .. +32767          */
+typedef signed long           sint32_t;        /* -2147483648 .. +2147483647     */
+typedef unsigned long         uint32_t;        /*           0 .. 4294967295      */
+typedef float                 float32_t;
+typedef double                float64_t;
+
+
+#ifdef PLATFORM_SUPPORT_SINT64_UINT64 /*Valid only for gnu and C99 */
+typedef signed    long long  sint64;   /* -9223372036854775808 .. 9223372036854775807      */
+typedef unsigned  long long  uint64;   /*                    0 .. 18446744073709551615     */
+
+typedef signed    long long  sint64_t;   /* -9223372036854775808 .. 9223372036854775807      */
+typedef unsigned  long long  uint64_t;   /*                    0 .. 18446744073709551615     */
+#endif
+
+#endif  /* PLATFORM_TYPES_H */
