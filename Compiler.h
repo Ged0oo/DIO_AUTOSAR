ffc2d58fa37b1036bf8ea1d796bdd3ed4d4e7adb
Mohamed Nagy <106141020+Ged0oo@users.noreply.github.com>
2023-09-11 14:11:06 +0300

    Update Compiler.h

--- Compiler.h
+++ Compiler.h
@@ -1,26 +1,29 @@
  /******************************************************************************
- * @File :				Compiler.h
- * @Version :			1.0.0
- * @Module: 			Dio
- * @brief : 			Header file for Dio Module.
- * @Author : 			Mohamed Nagy
- ******************************************************************************/
- 
-  /******************************************************************************
- * Project :			AUTOSAR r22-11 MCAL
- * Platform :			ARM
- * Board : 				STM32F103C8T6
- * Autosar Version :	4.4.0
- * SW Version : 		1.0.0
+ * @File :				Compiler.c                                             *
+ * @Version :			1.0.0                                                  *
+ * @Module: 			Dio                                                    *
+ * @brief : 			Header file for Dio Module.                            *
+ * @Author : 			Mohamed Nagy                                           *
  ******************************************************************************/
 
-#ifndef __COMPILER_H__
-#define __COMPILER_H__
+ /******************************************************************************
+ * Project :			AUTOSAR 4.4.0 MCAL 								       *
+ * Platform :			AVR                 								   *
+ * Board : 				ATMEGA32       								           *
+ * Autosar Version :	4.4.0               								   *
+ * SW Version : 		1.0.0               								   *
+ ******************************************************************************/
+
+#ifndef COMPILER_H
+#define COMPILER_H
+
 
 /*
- * @brief COMPILER VENDOR ID 
+ * Id for the company in the AUTOSAR
+ * for example GCC's ID = 1000
  */
-#define COMPILER_VENDOR_ID                                  (1U)
+#define COMPILER_VENDOR_ID                                  (1000U)
+
 
 /*
  * Module Version 1.0.0
@@ -29,6 +32,7 @@
 #define COMPILER_SW_MINOR_VERSION                           (0U)
 #define COMPILER_SW_PATCH_VERSION                           (0U)
 
+
 /*
  * AUTOSAR Version 4.4.0
  */
@@ -36,25 +40,41 @@
 #define COMPILER_AR_RELEASE_MINOR_VERSION                   (4U)
 #define COMPILER_AR_RELEASE_PATCH_VERSION                   (0U)
 
-/* The memory class is used for the declaration of local pointers */
-#define AUTOMATIC
 
-/* The memory class is used within type definitions, where no memory
-   qualifier can be specified */
+/*
+ * The memory class is used for the declaration of local pointers
+ */
+#define AUTOMATIC 
+
+
+/* The memory class is used within type definitions,
+ * where no memory qualifier can be specified
+ */
 #define TYPEDEF
 
-/* This is used to define the void pointer to zero definition */
+
+/*
+ * This is used to define the void pointer to zero definition
+ */
 #define NULL_PTR          ((void *)0)
 
-/* This is used to define the abstraction of compiler keyword inline */
+
+/*
+ * This is used to define the abstraction of compiler keyword inline
+ */
 #define INLINE            inline
 
-/* This is used to define the local inline function */
+
+/*
+ *  This is used to define the local inline function
+ */
 #define LOCAL_INLINE      static inline
 
-/* This is used to define the abstraction of compiler keyword static */
+
+/*
+ * This is used to define the abstraction of compiler keyword static
+ */
 #define STATIC            static
 
 
-#endif //__COMPILER_H__
-
+#endif
