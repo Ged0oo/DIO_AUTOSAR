53510ae25e60e8129ccab8ba2e013d46c8823c62
Mohamed Nagy <106141020+Ged0oo@users.noreply.github.com>
2023-09-11 14:12:54 +0300

    Update MemMap.h

--- MemMap.h
+++ MemMap.h
@@ -1,45 +1,60 @@
-/******************************************************************************
- * @File :				MemMap.h
- * @Version :			1.0.0
- * @Module: 			Dio
- * @brief : 			Header file for Dio Module.
- * @Author : 			Mohamed Nagy
- ******************************************************************************/
- 
  /******************************************************************************
- * Project :			AUTOSAR r22-11 MCAL
- * Platform :			ARM
- * Board : 				STM32F103C8T6
- * Autosar Version :	4.4.0
- * SW Version : 		1.0.0
+ * @File :				MemMap.h                                               *
+ * @Version :			1.0.0                                                  *
+ * @Module: 			Dio                                                    *
+ * @brief : 			Header file for Dio Module.                            *
+ * @Author : 			Mohamed Nagy                                           *
  ******************************************************************************/
- 
-#ifndef __MEM_MAP_H__
-#define __MEM_MAP_H__
 
+ /******************************************************************************
+ * Project :			AUTOSAR 4.4.0 MCAL 								       *
+ * Platform :			AVR                 								   *
+ * Board : 				ATMEGA32       								           *
+ * Autosar Version :	4.4.0               								   *
+ * SW Version : 		1.0.0               								   *
+ ******************************************************************************/
+
+/* =====================================================================================================
+ * 											 FILE GUARD
+ * =====================================================================================================*/
+
+#ifndef DIO_REGS_H
+#define DIO_REGS_H
+
+
+/* =====================================================================================================
+ * 										VERSION DEFINITION
+ * =====================================================================================================*/
 
 /*
  * Module Version 1.0.0
  */
-#define MEM_MAP_SW_MAJOR_VERSION           (1U)
-#define MEM_MAP_SW_MINOR_VERSION           (0U)
-#define MEM_MAP_SW_PATCH_VERSION           (0U)
+#define DIO_REGS_SW_MAJOR_VERSION           (1U)
+#define DIO_REGS_SW_MINOR_VERSION           (0U)
+#define DIO_REGS_SW_PATCH_VERSION           (0U)
+
 
 /*
- * AUTOSAR Version 4.4.0
+ * AUTOSAR Version 4.0.3
  */
-#define MEM_MAP_AR_RELEASE_MAJOR_VERSION   (4U)
-#define MEM_MAP_AR_RELEASE_MINOR_VERSION   (4U)
-#define MEM_MAP_AR_RELEASE_PATCH_VERSION   (0U)
+#define DIO_REGS_AR_RELEASE_MAJOR_VERSION   (4U)
+#define DIO_REGS_AR_RELEASE_MINOR_VERSION   (0U)
+#define DIO_REGS_AR_RELEASE_PATCH_VERSION   (3U)
+
 
 #include "Std_Types.h"
+
 
 /* =====================================================================================================
  * 										VERSION CHECK
  * =====================================================================================================*/
-/* AUTOSAR checking between Std_Types and Dio Modules */
-#if ((STD_TYPES_AR_RELEASE_MAJOR_VERSION != MEM_MAP_AR_RELEASE_MAJOR_VERSION)\
- ||  (STD_TYPES_AR_RELEASE_MINOR_VERSION != MEM_MAP_AR_RELEASE_MINOR_VERSION))
+
+/*
+ * AUTOSAR checking between Std_Types and Dio Modules
+ */
+#if ((STD_TYPES_AR_RELEASE_MAJOR_VERSION != DIO_REGS_AR_RELEASE_MAJOR_VERSION)\
+ ||  (STD_TYPES_AR_RELEASE_MINOR_VERSION != DIO_REGS_AR_RELEASE_MINOR_VERSION)\
+ ||  (STD_TYPES_AR_RELEASE_PATCH_VERSION != DIO_REGS_AR_RELEASE_PATCH_VERSION))
   #error "The AR version of Std_Types.h does not match the expected version"
 #endif
 
@@ -61,21 +76,22 @@ typedef union PORT_Union
 }PORT_Union;
 
 
-#define 	PORTA          *((volatile uint8 *)(0X3B))
-#define 	PORTB          *((volatile uint8 *)(0X38))
-#define 	PORTC          *((volatile uint8 *)(0X35))
-#define 	PORTD          *((volatile uint8 *)(0X32))
-	
-#define 	PINA           *((volatile uint8 *)(0X39))
-#define 	PINB           *((volatile uint8 *)(0X36))
-#define 	PINC           *((volatile uint8 *)(0X33))
-#define 	PIND           *((volatile uint8 *)(0X30))
-	
-#define 	DDRA           *((volatile uint8 *)(0X3A))
-#define 	DDRB           *((volatile uint8 *)(0X37))
-#define 	DDRC           *((volatile uint8 *)(0X34))
-#define 	DDRD           *((volatile uint8 *)(0X31))
+#define PORTA          *((volatile uint8 *)(0X3B))
+#define PORTB          *((volatile uint8 *)(0X38))
+#define PORTC          *((volatile uint8 *)(0X35))
+#define PORTD          *((volatile uint8 *)(0X32))
 
-#endif //__MEM_MAP_H__
 
- 
+#define PINA           *((volatile uint8 *)(0X39))
+#define PINB           *((volatile uint8 *)(0X36))
+#define PINC           *((volatile uint8 *)(0X33))
+#define PIND           *((volatile uint8 *)(0X30))
+
+
+#define DDRA          *((volatile uint8 *)(0X3A))
+#define DDRB          *((volatile uint8 *)(0X37))
+#define DDRC          *((volatile uint8 *)(0X34))
+#define DDRD          *((volatile uint8 *)(0X31))
+
+
+#endif /* DIO_REGS_H */
